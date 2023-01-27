922a8dc5a8d914437e9cac3fe7dfbecb7d9543bf
hawa khleifi <khleifihawa@gmail.com>
2023-01-27 14:19:12 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Form from './component/Form';
+import Navbar from './component/Navbar';
+import './Style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
